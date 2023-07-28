c8a49e117e0d890ed72192eb157734eff578fee6
Nachiketh-vn <108194089+Nachiketh-vn@users.noreply.github.com>
2023-07-28 10:01:13 +0530

    this is jQuery

---
+++ script.js
@@ -0,0 +1,47 @@
+$(".home,.about,.services,.portfolio,.contact").on({
+    "mouseover":function(){
+    $(this).css("background-color","#c3003e");    //here this means .home
+    },
+
+    "mouseout":function(){
+    $(this).css("background-color","");}    //here this means .home
+
+});
+
+$(".skills").css("background-color","#c3003e").css("border-radius","7px").css("padding","3px");
+$(".skills-contents").css("display","block");
+
+$(".skills").on({
+    "click":function(){
+    $(".education,.experence").css("background-color","");
+    $(this).css("background-color","#c3003e").css("border-radius","7px").css("padding","3px");
+    $(".skills-contents").css("display","block");
+    $(".education-contents,.experence-contents").css("display","none");
+    }
+});
+
+$(".education").on({
+    "click":function(){
+    $(".experence,.skills").css("background-color","");
+    $(this).css("background-color","#c3003e").css("border-radius","7px").css("padding","3px");
+    $(".education-contents").css("display","block");
+    $(".skills-contents,.experence-contents").css("display","none");
+    }
+});
+
+$(".experence").on({
+    "click":function(){
+    $(".education,.skills").css("background-color","");
+    $(this).css("background-color","#ff004f").css("border-radius","7px").css("padding","3px");
+    $(".experence-contents").css("display","block");
+    $(".education-contents,.skills-contents").css("display","none");
+    } 
+});
+
+$(".fa-bars").on("click",function(){
+    $("nav ul").css("right","0");
+});
+
+$(".fa-circle-xmark").on("click",function(){
+    $("nav ul").css("right","-200px");
+});
